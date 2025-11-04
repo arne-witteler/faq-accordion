68a84609e521cd5c24dfae0c58767fad39277fd4
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2025-11-04 14:28:03 +0100

    replace divs with buttons and add aria-expanded for accordion

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ questions.forEach((question) => {
   question.addEventListener("click", () => {
     const item = question.closest(".accordion__item");
     item.classList.toggle("is-active");
+
+    const expanded = item.classList.contains("is-active");
+    question.setAttribute("aria-expanded", expanded);
   });
 });
